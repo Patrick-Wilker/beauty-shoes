a0b1c9469ec0943b31e54652e3ed822ea018ec59
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-09-01 09:54:48 -0300

    Check quantity of products in stock

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -8,11 +8,21 @@ import {addToCartSuccess, updateAmount} from './actions';
 function* addToCart({id}){
     const productExists = yield select(
         state => state.cart.find(p => p.id === id)
-    )
+    );
+
+    const stock = yield call(api.get, `/stock/${id}`);
+
+    const stockAmount = stock.data.amount;
+    const currentAmount = productExists ? productExists.amount : 0;
+
+    const amount = currentAmount +1;
+
+    if(amount > stockAmount){
+        console.tron.warn('ERRO');
+        return;
+    }
 
     if(productExists){
-        const amount = productExists.amount + 1;
-
         yield put(updateAmount(id, amount));
     }else{
         const response = yield call(api.get, `/products/${id}`);
@@ -21,7 +31,7 @@ function* addToCart({id}){
             ...response.data,
             amount: 1,
             priceFormatted: formatPrice(response.data.price)
-        }
+        };
 
         yield put(addToCartSuccess(data));
     }
